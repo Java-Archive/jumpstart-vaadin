1c384820e44a03010da181f647043fc0bacfa91a
Sven Ruppert <sven.ruppert@gmail.com>
2015-12-10 17:49:58 +0100

    JMS-11 Add Metrics - Proxies to the REST / Servlets

--- modules/application/logic/src/main/java/org/rapidpm/jumpstart/vaadin/logic/properties/PropertyService.java
+++ modules/application/logic/src/main/java/org/rapidpm/jumpstart/vaadin/logic/properties/PropertyService.java
@@ -3,7 +3,7 @@ package org.rapidpm.jumpstart.vaadin.logic.properties;
 /**
  * Created by svenruppert on 07.12.15.
  */
-public interface PropertyService {
+public interface PropertyService  {
   String resolve(String key);
   boolean hasKey(String keyy);
 }
