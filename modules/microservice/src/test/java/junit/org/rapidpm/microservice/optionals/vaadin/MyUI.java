9fa68f46b4be313c0b5371319a9ff19b96c9e06d
Sven Ruppert <sven.ruppert@gmail.com>
2016-05-20 09:22:39 +0200

    JMS-36 move module - vaadin to the project vaadin-jumpstart

---
+++ modules/microservice/src/test/java/junit/org/rapidpm/microservice/optionals/vaadin/MyUI.java
@@ -0,0 +1,19 @@
+package junit.org.rapidpm.microservice.optionals.vaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+import static com.sun.deploy.trace.TraceLevel.UI;
+
+/**
+ *
+ */
+@Theme("valo")
+public class MyUI extends UI {
+
+  @Override
+  protected void init(final VaadinRequest request) {
+
+  }
+}
